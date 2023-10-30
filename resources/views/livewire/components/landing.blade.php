68df272bbde12696852b9f1489f2e5664e0e56fa
Leo <leonardoddavid8@gmail.com>
2023-10-30 03:01:34 -0500

    landing

--- resources/views/livewire/components/landing.blade.php
+++ resources/views/livewire/components/landing.blade.php
@@ -4,9 +4,9 @@
         <p class="text-lg">Elige tu perfil para empezar a utilizar la plataforma.</p>
     </div>
     <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
-        <div class="p-4 text-center border border-gray-300 hover:border-bluePrimary transition duration-300 rounded-lg" wire:click="selectOption"">
-        <img src="{{ asset('img/tipo-19.svg') }}" class="mb-4 mx-auto">
-        <p class="text-center font-normal pb-6">Autónomo/Empresa</p>
+        <div class="p-4 text-center border border-gray-300 hover:border-bluePrimary transition duration-300 rounded-lg sm:whitespace-normal whitespace-nowrap" wire:click="selectOption">
+            <img src="{{ asset('img/tipo-19.svg') }}" class="mb-4 mx-auto">
+        <p class="text-center font-normal pb-6">Autónomo /</br>Empresa</p>
     </div>
     <div class="p-4 text-center border border-gray-300 hover:border-bluePrimary transition duration-300 rounded-lg" wire:click="selectOption">
         <img src="{{ asset('img/tipo-18.svg') }}" class="mb-4 mx-auto">
